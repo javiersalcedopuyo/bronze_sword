14ba8bb5a275d47ca013130085c35962a59203d4
Javier Salcedo <javiersalcedo.p@gmail.com>
2023-05-15 22:53:38 +0100

    Draw a static background

--- src/game.zig
+++ src/game.zig
@@ -2,6 +2,7 @@ const raylib = @cImport({
     @cInclude("raylib.h");
 });
 const Vector2 = raylib.Vector2;
+const Rectangle = raylib.Rectangle;
 
 pub const Game = struct
 {
@@ -10,24 +11,61 @@ pub const Game = struct
     title: []const u8,
     window_size: Vector2,
 
+    background: raylib.Texture = undefined,
+
     pub fn init(self: *Self) void
     {
         raylib.InitWindow(@floatToInt(c_int, self.window_size.x),
                           @floatToInt(c_int, self.window_size.y),
                           self.title.ptr);
         raylib.SetTargetFPS(60);
+
+        self.background = raylib.LoadTexture( "assets/background2.png" );
     }
 
-    pub fn deinit(_: *Self) void
+    pub fn deinit(self: *Self) void
     {
+        raylib.UnloadTexture( self.background );
         raylib.CloseWindow();
     }
 
-    pub fn run_game_loop(_: *Self) void
+    pub fn run_game_loop(self: *Self) void
     {
         raylib.BeginDrawing();
         raylib.ClearBackground(raylib.SKYBLUE);
-        raylib.DrawText("Fresh Restart!", 0, 0, 32, raylib.BLACK);
+
+        self.DrawBackground();
+
         raylib.EndDrawing();
+    }
+
+    fn DrawBackground(self: *Self) void
+    {
+        const src_rect = Rectangle
+        {
+            .x = 0,
+            .y = 0,
+            .width  = @intToFloat( f32, self.background.width ),
+            .height = @intToFloat( f32, self.background.height )
+        };
+        const dst_rect = Rectangle
+        {
+            .x = 0,
+            .y = 0,
+            .width = self.window_size.x,
+            .height = self.window_size.y
+        };
+
+        const origin = Vector2{ .x=0, .y=0 };
+
+        raylib.DrawTexturePro
+        (
+            self.background,
+            src_rect,
+            dst_rect,
+            origin,
+            0, // rotation
+            raylib.LIGHTGRAY // tint
+        );   
     }
 };
